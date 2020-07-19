e30b8058d286b4596a72749b5ecdd3fefa989478
raincoat03 <jahab@naver.com>
2020-07-19 20:36:33 +0900

    naver 완성

--- naver_google.py
+++ naver_google.py
@@ -8,8 +8,11 @@ import time
 import lxml
 import pandas as pd
 import numpy as np
+import openpyxl
 from operator import itemgetter
+from datetime import datetime
 start = time.time()
+date = datetime.today()
 
 driver = webdriver.Chrome("C:/Users/june/Desktop/chromedriver.exe")
 url_naver = "https://searchad.naver.com/login"
@@ -17,6 +20,14 @@ id_naver = "lyrical98"
 password_naver = "dmsgur!23"
 search_total = []
 naver_total = []
+wb = openpyxl.Workbook()
+wb.save("keyword.xlsx")
+sheet = wb.active
+sheet["A2"] = "keyword"
+sheet["B2"] = "M+PC 클릭수"
+sheet["C2"] = "예상 클릭수"
+sheet["D2"] = "입찰가"
+sheet["E2"] = "예상비용"
 
 driver.get(url_naver)
 id_xpath = "/html/body/marvel-root/login/div/div/div/div/fieldset/dl/dd[1]/input"
@@ -103,62 +114,47 @@ for i in [3]: # , 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 
 search_total.sort(key=lambda x:-x[1])  # 클릭수 높은 순서대로 정렬
 
 # 목록 입력
-search_range_start = 0  # search_range는 0 ~ 179까지 가능. 한 번에 100개씩임.
-range_end = 178     # range_end는 178(한 번) ~ 0(179번)까지 가능.
-for i in range(179-range_end):
-    search_total_slice = search_total[(search_range_start)*100:(search_range_start+1)*100]
-    print(search_total_slice)
-    search_range_start += 1
-    search_enter = []
-    for j in search_total_slice:
-        search_enter.append(j[0])
+for i in range(len(search_total)):
     driver.get(keyword_naver_url)
     time.sleep(1)
     keyword_enter_sheet = "/html/body/elena-root/elena-wrap/div/div[2]/elena-tool-wrap/div/div/div/div/elena-keyword-planner/div[2]/div[2]/div/div/form/div[1]/textarea"
     driver.find_element_by_xpath(keyword_enter_sheet).click()
     keyword_enter = driver.find_element_by_xpath(keyword_enter_sheet)
     time.sleep(1)
-    for k in search_enter:
-        keyword_enter.send_keys(k)
-        keyword_enter.send_keys(Keys.ENTER)
+    keyword_enter.send_keys(search_total[i][0])
     time.sleep(1)
     driver.find_element_by_xpath("/html/body/elena-root/elena-wrap/div/div[2]/elena-tool-wrap/div/div/div/div/elena-keyword-planner/div[2]/div[2]/div/div/form/div[2]/button[2]").click()
-    time.sleep(3)
+    time.sleep(2)
     bid_price = driver.find_element_by_xpath("/html/body/elena-root/elena-wrap/div/div[2]/elena-tool-wrap/div/div/div/div/elena-keyword-estimate/div[3]/div[2]/div/div[1]/div[1]/div/elena-input-amt/div/input")
-    pre, pre_past = [], []
-
-    for i in range(70, 100001, 10):
-        n = i
+    print(search_total[i][1])
+    for j in range(70, 100001, 50):
+        n = j
         bid_price.click()
         bid_price.clear()
-        bid_price.send_keys(i)
-        time.sleep(1)
+        bid_price.send_keys(j)
         driver.find_element_by_xpath("/html/body/elena-root/elena-wrap/div/div[2]/elena-tool-wrap/div/div/div/div/elena-keyword-estimate/div[3]/div[2]/div/div[1]/div[4]/button").click()
-        time.sleep(3)
-        expect_name, expect_click, expect = [], [], []
+        time.sleep(2)
         html = driver.page_source
         soup = BeautifulSoup(html, 'html.parser')
-        name = soup.select("td.elenaColumn-keyword")
-        for i in name:
-            i = i.text.strip()
-            expect_name.append(i)
         click = soup.select("td.elenaColumn-clicks")
+        cost = soup.select("td.elenaColumn-cost")
+        cost_list = []
         for j in click:
             j = j.text.strip()
             j = j.replace(",", "")
             j = int(j)
-            expect_click.append(j)
-        for k in range(len(name)):
-            expect.append([expect_name[k],expect_click[k]])
-
-        for i in range(100):
-            if expect[i][1] < search_total_slice[i][1] + 30:
-                expect[i][1] = expect[i][1]
-            elif expect[i][1] >= search_total_slice[i][1] + 30:
-                expect[i][1] += 0
-
-        if pre == expect:
+            expect_click = j
+        for k in cost:
+            k = k.text.strip()
+            k = k.strip("원")
+            k = k.replace(",", "")
+            k = k.strip()
+            k = int(k)
+            cost_list.append(k)
+            print(cost_list)
+        if j >= search_total[i][1] + 30:
+            naver_total.append([search_total[i][0],search_total[i][1],j,n,cost_list[0]])
             break
-        else:
-            pre = expect
-        print(pre)
+        time.sleep(1)
+    sheet.append(naver_total[-1])
+    wb.save("keyword.xlsx")
